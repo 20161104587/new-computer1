dfecbec5eac7e4b7d616ba20d329f67e89ec7971
20161104587 <a20161104587@chu1e1adeimac.local>
2018-10-10 10:23:30 +0800

    Initial Commit

---
+++ new computer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new computer
+//
+//  Created by 20161104587 on 2018/10/10.
+//  Copyright © 2018年 20161104587. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
