ce8f6c2701d9dde096297c02febf9c06d0bc905f
20161104587 <a20161104587@chu1e1adeiMac.local>
2018-11-07 11:51:21 +0800

    777

--- new computer/ViewController.swift
+++ new computer/ViewController.swift
@@ -22,116 +22,73 @@ class ViewController: UIViewController {
     
     var add = 0 
     
-    var re = 0//判断result.text前是否存在符号
     @IBAction func number1(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "1"
-            
-        }else{
+        
             
             result.text = result.text! + "1"
             
-        }
+        
     }
     @IBAction func number2(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "2"
-            
-        }else{
+       
             
             result.text = result.text! + "2"
             
-        }
+        
     }
     @IBAction func number3(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "3"
-            
-        }else{
+       
             
             result.text = result.text! + "3"
             
-        }
+        
     }
     @IBAction func number4(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "4"
-            
-        }else{
+        
             
             result.text = result.text! + "4"
             
-        }
+        
     }
     @IBAction func number5(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "5"
-            
-        }else{
+        
             
             result.text = result.text! + "5"
             
-        }
+        
     }
     @IBAction func number6(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "6"
-            
-        }else{
+        
             
             result.text = result.text! + "6"
             
-        }
+        
     }
     @IBAction func number7(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "7"
-            
-        }else{
+        
             
             result.text = result.text! + "7"
             
-        }
+        
     }
     @IBAction func number8(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "8"
-            
-        }else{
+        
             
             result.text = result.text! + "8"
             
-        }
+        
     }
     @IBAction func number9(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "9"
-            
-        }else{
+       
             
             result.text = result.text! + "9"
             
-        }
+        
     }
     @IBAction func number0(_ sender: Any) {
-        if re == 1{
-            
-            result.text = "0"
-            
-        }else{
-            
+      
             result.text = result.text! + "0"
-            
-        }
+        
     }
     @IBAction func dot(_ sender: Any) {
         result.text = result.text! + "."
@@ -156,7 +113,7 @@ class ViewController: UIViewController {
             
             number = 2
             
-            re = 1
+        
             
         }else{
             
@@ -174,7 +131,7 @@ class ViewController: UIViewController {
                 
                 number = 2
                 add = 1
-                re = 0
+               
                 
             }
             
@@ -196,7 +153,7 @@ class ViewController: UIViewController {
             
             number = 1
             
-            re = 1
+           
             
         }else{
             
@@ -214,7 +171,7 @@ class ViewController: UIViewController {
                 
                 number = 1
                 
-                re = 0
+             
                 add = 1
                 
             }
@@ -237,7 +194,7 @@ class ViewController: UIViewController {
             
             number = 3
             
-            re = 1
+          
             
         }else{
             
@@ -255,7 +212,7 @@ class ViewController: UIViewController {
                 
                 number = 3
                 
-                re = 0
+               
                 
                 add = 1
                 
@@ -279,7 +236,7 @@ class ViewController: UIViewController {
             
             number = 4
             
-            re = 1
+        
             
         }else{
             
@@ -297,7 +254,7 @@ class ViewController: UIViewController {
                 
                 number = 4
                 
-                re = 0
+             
                 
                 add = 1
                 
@@ -311,14 +268,16 @@ class ViewController: UIViewController {
     @IBAction func clear(_ sender: Any) {
         result.text = ""
         
-        re = 0
+       
     }
+    
+    
+    
     @IBAction func equal(_ sender: Any) {
         var d:Double
         
         var c:Double
-        
-                                                                            let x = Double(result_1.text!)!
+        let x = Double(result_1.text!)!
         
         c = (result.text! as NSString).doubleValue
         
@@ -356,7 +315,7 @@ class ViewController: UIViewController {
             
         }
         
-        re = 1
+     
         
         judge = 0
         
